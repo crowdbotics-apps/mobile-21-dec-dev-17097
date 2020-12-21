5579a003db2367694ccfdea337d714f1933ed808
Crowdbotics <team@crowdbotics.com>
2020-12-21 11:59:56 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const mobiledecAPI = axios.create({
-  baseURL: "https://mobile-21-dec-dev-17097-prod.herokuapp.com/",
+  baseURL: "https://mobile-21-dec-dev-17097.botics.co/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function api_v1_customtext_list(action) {
