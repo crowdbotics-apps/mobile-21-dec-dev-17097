19f2d3d295732136c624bf2abc8f0ebbb93e840b
Crowdbotics <team@crowdbotics.com>
2020-12-21 12:07:22 +0000

    Update components

--- src/features/BlankScreen016004/screens/blank.js
+++ src/features/BlankScreen016004/screens/blank.js
@@ -24,11 +24,45 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = {
+    CheckBox_73: true,
+    Switch_140: true,
+    TextInput_146: "",
+    DateTimePicker_157: new Date(""),
+    CheckBox_5: true,
+    CheckBox_7: true,
+    DateTimePicker_150: new Date("")
+  }
 
   render = () => (
     <View>
+      <CheckBox
+        title="Checkbox"
+        checked={this.state.CheckBox_73}
+        onPress={nextChecked => this.setState({ CheckBox_73: nextChecked })}
+      />
       <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Switch
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+        style={styles.Switch_140}
+        value={this.state.Switch_140}
+        onValueChange={nextChecked =>
+          this.setState({ Switch_140: nextChecked })
+        }
+      />
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_146}
+        onChangeText={nextValue => this.setState({ TextInput_146: nextValue })}
+      />
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_157}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_157: selectedDate })
+        }
+      />
       <Text>Sample text content</Text>
       <Image
         resizeMode="contain"
@@ -38,13 +72,40 @@ export default class Blank extends React.Component {
         }}
         style={styles.Image_4}
       />
+      <CheckBox
+        title="Radio button"
+        disabled={false}
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        containerStyle={styles.CheckBox_5}
+        checked={this.state.CheckBox_5}
+        onPress={nextChecked => this.setState({ CheckBox_5: nextChecked })}
+      />
+      <CheckBox
+        title="Radio button"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={this.state.CheckBox_7}
+        onPress={nextChecked => this.setState({ CheckBox_7: nextChecked })}
+      />
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_150}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_150: selectedDate })
+        }
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   View_1: {},
+  CheckBox_73: {},
   Button_2: {},
+  Switch_140: { alignSelf: "flex-start" },
+  TextInput_146: {},
+  DateTimePicker_157: {},
   Text_3: {},
   Image_4: {
     width: 100,
@@ -56,5 +117,21 @@ const styles = StyleSheet.create({
     borderTopWidth: 6,
     borderBottomWidth: 6,
     borderRadius: 17
-  }
+  },
+  CheckBox_5: {
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+  CheckBox_7: {},
+  DateTimePicker_150: {}
 })
